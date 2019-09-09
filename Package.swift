7286bfa8453e3b062ec0ce8951f9ddc7e2f9608e
Franciszek Gonciarz <franciszek@scandit.com>
2019-09-09 12:37:15 +0200

    Lower platform requirement to iOS 10

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NFCPassportReader",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
