fd1d0bb0923b3f7c96f82ea2f6813f09700fecc9
AndyQ <andy.qua@gmail.com>
2019-10-29 12:48:25 +0000

    Removed passiveAuthentication from Example app Fixed bug where model changes weren't correctly sent to SwiftUI

--- Example/NFCPassportReaderApp/Model/PassportDetails.swift
+++ Example/NFCPassportReaderApp/Model/PassportDetails.swift
@@ -9,25 +9,14 @@
 import SwiftUI
 import Combine
 
-
-
 class PassportDetails : ObservableObject {
-    var willChange = PassthroughSubject<Void, Never>()
-    
-    var passportNumber : String = UserDefaults.standard.string(forKey:"passportNumber" ) ?? "" { didSet { update() } }
-    var dateOfBirth: String = UserDefaults.standard.string(forKey:"dateOfBirth" ) ?? "" { didSet { update() } }
-    var expiryDate: String = UserDefaults.standard.string(forKey:"expiryDate" ) ?? "" { didSet { update() } }
-    
-    var passport : Passport? {
-        willSet { update() }
-    }
+    @Published var passportNumber : String = UserDefaults.standard.string(forKey:"passportNumber" ) ?? ""
+    @Published var dateOfBirth: String = UserDefaults.standard.string(forKey:"dateOfBirth" ) ?? ""
+    @Published var expiryDate: String = UserDefaults.standard.string(forKey:"expiryDate" ) ?? ""
+    @Published var passport : Passport?
     
     var isValid : Bool {
         return passportNumber.count >= 8 && dateOfBirth.count == 6 && expiryDate.count == 6
-    }
-    
-    func update() {
-        willChange.send(())
     }
     
     func getMRZKey() -> String {
