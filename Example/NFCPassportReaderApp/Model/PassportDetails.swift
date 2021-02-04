6e24230e1938bd057b5bed88ec9950c72024893b
AndyQ <andy.qua@gmail.com>
2021-02-04 14:39:58 +0000

    Fix to SampleApp - to actually use the padded fields rather than using them for the checksum calculation only!

--- Example/NFCPassportReaderApp/Model/PassportDetails.swift
+++ Example/NFCPassportReaderApp/Model/PassportDetails.swift
@@ -26,25 +26,35 @@ class PassportDetails : ObservableObject {
         d.set(dateOfBirth, forKey: "dateOfBirth")
         d.set(expiryDate, forKey: "expiryDate")
         
-        // Calculate checksums
-        let passportNrChksum = calcCheckSum(passportNumber, fieldLength:9)
-        let dateOfBirthChksum = calcCheckSum(dateOfBirth, fieldLength:6)
-        let expiryDateChksum = calcCheckSum(expiryDate, fieldLength:6)
+        // Pad fields if necessary
+        let pptNr = pad( passportNumber, fieldLength:9)
+        let dob = pad( dateOfBirth, fieldLength:6)
+        let exp = pad( expiryDate, fieldLength:6)
         
-        let mrzKey = "\(passportNumber)\(passportNrChksum)\(dateOfBirth)\(dateOfBirthChksum)\(expiryDate)\(expiryDateChksum)"
+        // Calculate checksums
+        let passportNrChksum = calcCheckSum(pptNr)
+        let dateOfBirthChksum = calcCheckSum(dateOfBirth)
+        let expiryDateChksum = calcCheckSum(expiryDate)
+
+        
+        let mrzKey = "\(pptNr)\(passportNrChksum)\(dob)\(dateOfBirthChksum)\(exp)\(expiryDateChksum)"
         
         return mrzKey
     }
     
-    func calcCheckSum( _ checkString : String, fieldLength:Int ) -> Int {
+    func pad( _ value : String, fieldLength:Int ) -> String {
+        // Pad out field lengths with < if they are too short
+        let paddedValue = (value + String(repeating: "<", count: fieldLength)).prefix(fieldLength)
+        return String(paddedValue)
+    }
+    
+    func calcCheckSum( _ checkString : String ) -> Int {
         let characterDict  = ["0" : "0", "1" : "1", "2" : "2", "3" : "3", "4" : "4", "5" : "5", "6" : "6", "7" : "7", "8" : "8", "9" : "9", "<" : "0", " " : "0", "A" : "10", "B" : "11", "C" : "12", "D" : "13", "E" : "14", "F" : "15", "G" : "16", "H" : "17", "I" : "18", "J" : "19", "K" : "20", "L" : "21", "M" : "22", "N" : "23", "O" : "24", "P" : "25", "Q" : "26", "R" : "27", "S" : "28","T" : "29", "U" : "30", "V" : "31", "W" : "32", "X" : "33", "Y" : "34", "Z" : "35"]
         
-        // Pad out field lengths with < if they are too short
-        let paddedCheckString = (checkString + String(repeating: "<", count: fieldLength)).prefix(fieldLength)
         var sum = 0
         var m = 0
         let multipliers : [Int] = [7, 3, 1]
-        for c in paddedCheckString {
+        for c in checkString {
             guard let lookup = characterDict["\(c)"],
                 let number = Int(lookup) else { return 0 }
             let product = number * multipliers[m]
