84b4c823ead7f7f61049302f4100ed21db32f945
Andy Qua <andy.qua@gmail.com>
2023-02-03 13:27:59 +0000

    Changed expected response length to -1 in TagReader:selectPassportApplication. Should have been part of @TimoTegtmeier's PR (#153) but missed! Thanks to @Thormeard for spotting this

--- Sources/NFCPassportReader/TagReader.swift
+++ Sources/NFCPassportReader/TagReader.swift
@@ -234,7 +234,7 @@ public class TagReader {
     func selectPassportApplication() async throws -> ResponseAPDU {
         // Finally reselect the eMRTD application so the rest of the reading works as normal
         Log.debug( "Re-selecting eMRTD Application" )
-        let cmd : NFCISO7816APDU = NFCISO7816APDU(instructionClass: 0x00, instructionCode: 0xA4, p1Parameter: 0x04, p2Parameter: 0x0C, data: Data([0xA0, 0x00, 0x00, 0x02, 0x47, 0x10, 0x01]), expectedResponseLength: 256)
+        let cmd : NFCISO7816APDU = NFCISO7816APDU(instructionClass: 0x00, instructionCode: 0xA4, p1Parameter: 0x04, p2Parameter: 0x0C, data: Data([0xA0, 0x00, 0x00, 0x02, 0x47, 0x10, 0x01]), expectedResponseLength: -1)
         
         let response = try await self.send( cmd: cmd)
         return response
