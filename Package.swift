9a22b0f6cf4df1a71a78c97a3bb7b8ee3bac11a0
Tomasz Kiszka <tomasz.kiszka@revolut.com>
2025-07-31 16:42:58 +0200

    Modify Package.swift to require OpenSSL 3.3.3001

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.2300"))
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "3.3.3001"))
 
     ],
     targets: [
