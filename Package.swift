17d8303703416bd935601c84d37ba80f386db677
Oscar Franco <ospfranco@gmail.com>
2025-12-11 14:49:25 +0100

    Undo unrelated changes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,6 @@
 // swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -8,14 +10,12 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NFCPassportReader",
-            targets: ["NFCPassportReader"])
+            targets: ["NFCPassportReader"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(
-            url: "https://github.com/krzyzanowskim/OpenSSL-Package.git",
-            .upToNextMinor(from: "3.3.1000"))
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL-Package.git", .upToNextMinor(from: "3.3.1000")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,12 +23,13 @@ let package = Package(
         .target(
             name: "NFCPassportReader",
             dependencies: [
-                .product(name: "OpenSSL", package: "OpenSSL-Package")
+              .product(name: "OpenSSL", package: "OpenSSL-Package")
             ]),
         .testTarget(
             name: "NFCPassportReaderTests",
             dependencies: [
-                .product(name: "OpenSSL", package: "OpenSSL-Package")
+              .product(name: "OpenSSL", package: "OpenSSL-Package")
             ]),
     ]
 )
+
