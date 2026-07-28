6e37f1ab249fef82771da46d32707f2b94ed090f
Andy Qua <andy.qua@gmail.com>
2026-07-28 11:02:48 +0100

    V2.3.3    Added Optional Data element to passport model - pulls out element 53 from DG1

--- Sources/NFCPassportReader/NFCPassportModel.swift
+++ Sources/NFCPassportReader/NFCPassportModel.swift
@@ -31,7 +31,8 @@ public class NFCPassportModel {
     public private(set) lazy var dateOfBirth : String = { return passportDataElements?["5F57"] ?? "?" }()
     public private(set) lazy var gender : String = { return passportDataElements?["5F35"] ?? "?" }()
     public private(set) lazy var nationality : String = { return passportDataElements?["5F2C"] ?? "?" }()
-    
+    public private(set) lazy var optionalData : String = { return passportDataElements?["53"] ?? "?" }()
+
     public private(set) lazy var lastName : String = {
         return names[0].replacingOccurrences(of: "<", with: " " )
     }()
